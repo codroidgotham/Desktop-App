aad6e4c0aed911e2b374006fadbffa0731b8c000
codroidgotham <no.one1970@protonmail.com>
2022-10-04 19:23:54 +0500

    still not parsing

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -23,15 +23,66 @@ export class PasswordComponent implements OnInit,DoCheck {
   ngDoCheck(): void {
    
   }
-  readGPO():number[]{
-    
-    const pwdContents=fs.readFileSync('../assets/group-policy.inf','utf8').split('\n').slice(3).slice(0,8)
-    const args=[]
-    pwdContents.map(
-      (content)=>args.push( parseInt(content.split("=")[1])   )
-    )
-      return args
+hex_to_ascii(str1)
+ {
+	var hex  = str1.toString();
+	var str = '';
+	for (var n = 0; n < hex.length; n += 2) {
+		str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+	}
+	return str;
+ }
+
+ utf8(text) {
+  var surrogate = encodeURIComponent(text);
+  var result = '';
+  for (var i = 0; i < surrogate.length;) {
+      var character = surrogate[i];
+  i += 1;
+      if (character == '%') {
+        var hex = surrogate.substring(i, i += 2);
+    if (hex) {
+      result += String.fromCharCode(parseInt(hex, 16));
+    }
+      } else {
+        result += character;
+      }
   }
+  return result;
+};
+
+  readGPO():any{
+    let pwdContents;
+    var newContents:string[]=[];
+    const args=[]
+    fetch('../assets/group-policy.inf').then(response=>response.text()).then(body=>{
+      pwdContents=body.split('\n').map(ele=>ele.replace(" ",'')).slice(3).slice(0,8)
+      // newContents=pwdContents.map(content=>{
+      //   newContents.push(content)
+      //   console.log(typeof(content))
+      console.log("in",pwdContents)
+      pwdContents.map(
+        (content)=>{
+          args.push( parseInt(content.split("=")[1].replace(" ",'')));
+        console.log("hh"+(content.split("=")[1].trim()
+        )
+        )
+      } 
+          )
+      
+      console.log("final",args)
+      return args;
+      })
+      
+      
+      
+     
+        
+    
+    };
+    
+    
+  
   check()
   {
     this.resPass=new Password(this.readGPO())
