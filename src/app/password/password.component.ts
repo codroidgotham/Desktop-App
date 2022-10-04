df3745847be6c0845d757f189c2c2b4efe52b399
codroidgotham <no.one1970@protonmail.com>
2022-10-04 20:10:13 +0500

    finaly reading

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,DoCheck,ViewChild,ViewContainerRef,ElementRef } from '@angular/core';
+import { Component, OnInit, DoCheck, ViewChild, ViewContainerRef, ElementRef } from '@angular/core';
 import * as fs from 'fs'
 import { Password } from '../password';
 
@@ -7,87 +7,62 @@ import { Password } from '../password';
   templateUrl: './password.component.html',
   styleUrls: ['./password.component.css']
 })
-export class PasswordComponent implements OnInit,DoCheck {
-  resPass:Password
-  status:boolean
-  @ViewChild('alert',{read:ElementRef})
-  alertBox:HTMLParagraphElement
-
+export class PasswordComponent implements OnInit, DoCheck {
+  resPass: Password
+  status: boolean
+  @ViewChild('alert1')
+  alertBox:ElementRef
+  container:HTMLElement
   constructor() { }
-  
-  ngOnInit(): void {
-    
 
+  ngOnInit(): void {
+    console.log("oninit")
+    //this.container=this.alertBox
+    console.log(this.alertBox)
   }
 
   ngDoCheck(): void {
-   
-  }
-hex_to_ascii(str1)
- {
-	var hex  = str1.toString();
-	var str = '';
-	for (var n = 0; n < hex.length; n += 2) {
-		str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
-	}
-	return str;
- }
 
- utf8(text) {
-  var surrogate = encodeURIComponent(text);
-  var result = '';
-  for (var i = 0; i < surrogate.length;) {
-      var character = surrogate[i];
-  i += 1;
-      if (character == '%') {
-        var hex = surrogate.substring(i, i += 2);
-    if (hex) {
-      result += String.fromCharCode(parseInt(hex, 16));
-    }
-      } else {
-        result += character;
-      }
   }
-  return result;
-};
+  
+ 
 
-  readGPO():any{
+  readGPO() {
     let pwdContents;
-    var newContents:string[]=[];
-    const args=[]
-    fetch('../assets/group-policy.inf').then(response=>response.text()).then(body=>{
-      pwdContents=body.split('\n').map(ele=>ele.replace(" ",'')).slice(3).slice(0,8)
+    var newContents: string[] = [];
+    const args = []
+    fetch('../assets/group-policy.inf').then(response => response.text()).then(body => {
+      pwdContents = body.split('\n').map(ele => ele.replace(" ", '')).slice(3).slice(0, 8)
       // newContents=pwdContents.map(content=>{
       //   newContents.push(content)
       //   console.log(typeof(content))
-      console.log("in",pwdContents)
+      console.log("in", pwdContents)
       pwdContents.map(
-        (content)=>{
-          args.push( parseInt(content.split("=")[1].replace(" ",'')));
-        console.log("hh"+(content.split("=")[1].trim()
-        )
-        )
-      } 
-          )
-      
-      console.log("final",args)
-      return args;
-      })
-      
-      
-      
-     
-        
-    
-    };
-    
-    
-  
-  check()
-  {
-    this.resPass=new Password(this.readGPO())
-    this.resPass.MinimumPasswordLength>=8 ? this.status=false:this.status=true;
-    if (this.status)     this.alertBox.style.color='green'
-    else this.alertBox.style.color='red'
+        (content) => {
+          args.push(parseInt(content.split("=")[1].replace(/\x00/g,"").replace('\r')));
+          //console.log("hh" + content.split("=")[1].replace(/\x00/g,"").replace('\r').length)
+          
+        }
+      )
+
+      console.log("final", (args[1]))
+      this.container=this.alertBox.nativeElement
+      this.resPass = new Password(args)
+    this.resPass.MinimumPasswordLength >= 8 ? this.status = true: this.status = false;
+    if (this.status) this.container.style.color='blue'
+    else this.container.style.color='red'
+    })
+
+
+
+
+
+
+  };
+
+
+
+  check() {
+    this.readGPO()
   }
 }
